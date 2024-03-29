fa339a72b12f7d856e0c49f6a657da20597ffd34
SelimBellagha <ms.bellagha@gmail.com>
2024-03-29 11:54:48 -0400

    Partie C ajout hash commit

--- myscript.py
+++ myscript.py
@@ -1,7 +1,7 @@
 import os
 import sys
-badhash = sys.argv[1]
-goodhash = sys.argv[2]
+badhash = "6256a66dfaf03a14874d79d34c30d03d29fb4eb0"
+goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 bisect_command = f"python manage.py test"
 os.system(f"git bisect start {badhash} {goodhash}")
 os.system(f"git bisect run {bisect_command}")
