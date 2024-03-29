15f5cbc4bfcc1c666b78d69a4c218163cdf8401c
SelimBellagha <ms.bellagha@gmail.com>
2024-03-29 12:08:03 -0400

    Partie C ajout hash commit actuel

--- myscript.py
+++ myscript.py
@@ -1,6 +1,7 @@
 import os
-import sys
-badhash = "6256a66dfaf03a14874d79d34c30d03d29fb4eb0"
+from pip._internal.vcs import git
+
+badhash = git.Repo(search_parent_directories=True).head.object.hexsha
 goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 bisect_command = f"python manage.py test"
 os.system(f"git bisect start {badhash} {goodhash}")
